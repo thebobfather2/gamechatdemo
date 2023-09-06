c6ee7f322b6e2a3666ba09f9393cd43c2c1c5a14
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-09-05 20:05:20 -0400

    format for games

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 function Home() {
 
   const iframeContainerStyle = {
-    width: '100%',
+    width: '80%',
     margin: '0 auto',
     position: 'relative',
     paddingBottom: '56.25%', // 16:9 aspect ratio (9 / 16 * 100%)
@@ -12,31 +12,26 @@ function Home() {
 
   return (
     <main className="MainContainer">
+      <section className="iframeContainer">
+        {/* Iframe 1 */}
 
-              {/* Iframe 2 */}
-              <div style={iframeContainerStyle}>
+        <div style={iframeContainerStyle}>
+          <iframe
+            src="https://www.miniplay.com/embed/mario-kart-64?volume=0"
+            title="IFrame 1"
+            id="iframe1"
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+          ></iframe>
+        </div>
+        {/* Iframe 2 */}
+        <div style={iframeContainerStyle}>
           <iframe
           src="https://vsnreact.herokuapp.com/VSIR"
             title="IFrame 2"
             id="iframe2"
             style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-            allow="autoplay" // Allow autoplay
-            autoplay // Autoplay attribute
           ></iframe>
         </div>
-        
-      <section className="iframeContainer">
-        {/* Iframe 1 */}
-        <div style={iframeContainerStyle}>
-          <iframe
-          src="https://www.miniplay.com/embed/mario-kart-64"
-            title="IFrame 1"
-            id="iframe1"
-            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-            muted // Add the muted attribute to mute the iframe
-          ></iframe>
-        </div>
-
       </section>
     </main>
   );
